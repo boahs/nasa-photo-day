d1954aa338672461b72e16aa47d80d4c5ae5e6aa
boahs <39522778+boahs@users.noreply.github.com>
2020-08-13 19:04:00 -0400

    first

---
+++ src/theme/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  mainColor: "#004d99",
+  secondaryColor: "#e60000",
+  headerColor: "#000000",
+  white: "white",
+  normalSize: "1rem",
+  smallSize: "0.8rem",
+  highlightedText: "yellow",
+  smallPadding: "3%",
+  transformUp: "scale(1.5)",
+};
+
+export default theme;
