0142efaee1448bfef13513d2813e98e4473f1c5a
boahs <39522778+boahs@users.noreply.github.com>
2020-08-17 23:14:20 -0400

    rvert

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=";
 export const API_KEY = "USRuqSU8OT3Szx3l7pP4qxpqEROVq4y5aQH0Pp3p";
-export const GIT_URL = "https://github.com/boahs/nasa-photo-day"
+export const GIT_URL = "https://github.com/boahs/nasa-photo-day";
